d55347deeece19f63ad55e8804c6b36003fd0425
chenxingyong <2103095120@qq.com>
2025-08-26 21:04:51 +0800

    用lambda表达式创建线程的语法

--- src/main/java/com/cxy/test/Test1.java
+++ src/main/java/com/cxy/test/Test1.java
@@ -4,7 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "c.Test1")
 public class Test1 {
-    public static void main(String[] args) {
+    public  void test2() {
+        Thread thread = new Thread(()->log.debug("running"));
+        thread.setName("施贝贝");
+        thread.start();
+    }
+    public void test1() {
         Thread thread = new Thread(){
             @Override
             public void run() {
